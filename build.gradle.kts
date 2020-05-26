a202c9a2dd30f8e09cdc16955baad33a402a75d3
Mark Sheehan <mark.sheehan@q-bot.co>
2020-05-26 18:42:14 +0100

    Mongodb has been a nice experiment, but getting a basic query in is hard

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+
 //	implementation("org.springframework.boot:spring-boot-starter-data-rest")
 	implementation("org.springframework.boot:spring-boot-starter-mustache")
 	implementation("org.springframework.boot:spring-boot-starter-web")
--- src/main/kotlin/net/marksheehan/shape/backend/service/ShapeServiceImpl.kt
+++ src/main/kotlin/net/marksheehan/shape/backend/service/ShapeServiceImpl.kt
@@ -3,8 +3,13 @@ package net.marksheehan.shape.backend.service
 import net.marksheehan.shape.backend.datamodel.Square
 import net.marksheehan.shape.backend.repository.ShapeRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.mongodb.core.MongoOperations
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
 import org.springframework.stereotype.Service
 import java.util.*
+
 
 @Service
 class ShapeServiceImpl(@Autowired val shapeRepository: ShapeRepository) : ShapeService {
@@ -20,12 +25,32 @@ class ShapeServiceImpl(@Autowired val shapeRepository: ShapeRepository) : ShapeS
     override fun createSquare(shape: Square)  : CreateShapeResponse{
         val result = shapeRepository.save(shape)
 
+        val b= shapeRepository.findAllSquares()
+
+        val results = shapeRepository.isSquareIntersectingAnotherSquare(shape.topLeft.x,
+                shape.topLeft.y,
+                shape.bottomRight.x,
+                shape.bottomRight.y)
+
+
+//        val query = Query()
+//
+//        query.addCriteria(Criteria.where("leftPoint.x").lt(0).gt(20))
+//        query.addCriteria(Criteria.where("leftPoint.x").lt(0).gt(50))
+//
+//        val mongoOps: MongoOperations = MongoTemplate(Mongo(), "database")
+//
+//        mongoOps.insert<Any>(Person("Joe", 34))
+//
+//        QueryBuilder.start()
+//
+//        query.addCriteria("\$and", andQuery)
+
         result.uniqueId?.let {
             return CreateShapeResponse(true, "Creation success, with id ${result.uniqueId}", it)
         }
 
         return CreateShapeResponse(false, "", null)
-
     }
 
 }
