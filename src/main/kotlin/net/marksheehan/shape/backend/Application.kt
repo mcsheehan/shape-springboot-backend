1769f5d47e1d72b4bd903f694d3f3430680f898b
Mark Sheehan <mark.sheehan@q-bot.co>
2020-05-27 14:27:11 +0100

    Added test results and reports to the artifacts

--- src/main/kotlin/net/marksheehan/shape/backend/Application.kt
+++ src/main/kotlin/net/marksheehan/shape/backend/Application.kt
@@ -5,13 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 
-//exclude = [MongoAutoConfiguration::class]
-//CommandLineRunner
 @SpringBootApplication
 @EnableAutoConfiguration
 class Application()  {
 }
-//@Autowired val repository: ShapeRepository
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
